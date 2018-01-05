7d509f8b9617c99e2bbc4cb48d59832c0f54a356
infamy <alex@pixelfactor.ca>
2018-01-05 01:51:53 -0800

    Fix pathing and add support for make graph-build

--- getconfig.sh
+++ getconfig.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 DID=$(sudo docker ps | grep "hassbuildroot" | awk '{ print $1 }')
-docker cp $DID:/buildroot/buildroot-2017.11.1/.config ~/hassbuild/vm/buildroot.config
-docker cp $DID:/buildroot/buildroot-2017.11.1/linux.config ~/hassbuild/vm/linux.config
+docker cp $DID:/buildroot/buildroot-2017.11.1/.config vm/buildroot.config
+docker cp $DID:/buildroot/buildroot-2017.11.1/linux.config vm/linux.config
 
