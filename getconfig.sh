95b32a4dd5e84d16eccf2ec7ecdf1c2a054a8ff4
Pascal Vizeli <pascal.vizeli@syshack.ch>
2018-03-04 17:01:09 +0100

    Update getconfig.sh

--- getconfig.sh
+++ getconfig.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 DID=$(docker ps | grep "hassbuildroot" | awk '{ print $1 }')
-docker cp $DID:/buildroot/buildroot-2017.11.1/.config vm/buildroot.config
-docker cp $DID:/buildroot/buildroot-2017.11.1/linux.config vm/linux.config
+docker cp $DID:/buildroot/buildroot-2017.11.2/.config vm/buildroot.config
+docker cp $DID:/buildroot/buildroot-2017.11.2/linux.config vm/linux.config
 
