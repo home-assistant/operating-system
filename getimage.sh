f2a3d7b697db7145b5d5b27906c3874f7b380387
Pascal Vizeli <pascal.vizeli@syshack.ch>
2018-03-04 17:00:28 +0100

    Update getimage.sh

--- getimage.sh
+++ getimage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir -p output
-DID=$(sudo docker ps | grep "hassbuildroot" | awk '{ print $1 }')
+DID=$(docker ps | grep "hassbuildroot" | awk '{ print $1 }')
 docker cp $DID:/buildroot/buildroot-2017.11.1/output/images/rootfs.iso9660 output/rootfs.iso
 docker cp $DID:/buildroot/buildroot-2017.11.1/output/graphs output/graphs
