c9ddb937ea1c9bd6c26a12e289aa531196d0ef61
Pascal Vizeli <pascal.vizeli@syshack.ch>
2018-03-17 19:05:45 +0100

    Update patches.sh

--- scripts/patches.sh
+++ scripts/patches.sh
@@ -3,6 +3,6 @@ set -e
 
 BUILDROOT_PATCHES="buildroot-patches/"
 
-for patch in "$BUILDROOT_PATCHES"; do
-      patch -d buildroot -p1 < $${patch};
+for patch_file in "$BUILDROOT_PATCHES"; do
+      patch -d buildroot/ -p1 < ${patch_file};
 done
