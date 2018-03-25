752a7d473eb29973b7e9dd45900f751b0f2cc8b9
Pascal Vizeli <pascal.vizeli@syshack.ch>
2018-03-25 13:52:23 +0200

    Delete getimage.sh

--- scripts/getimage.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mkdir -p output
-DID=$(docker ps | grep "hassbuildroot" | awk '{ print $1 }')
-docker cp $DID:/build/buildroot/output/images/rootfs.iso9660 output/rootfs.iso
-docker cp $DID:/build/buildroot/output/graphs output/graphs
