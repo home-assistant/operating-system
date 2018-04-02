9011e1226c5d6eab15e243de4a490512023ba9c7
Pascal Vizeli <pascal.vizeli@syshack.ch>
2018-04-02 22:56:03 +0200

    Update post-build.sh

--- buildroot-external/board/ova/post-build.sh
+++ buildroot-external/board/ova/post-build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+SCRIPT_DIR=${BR2_EXTERNAL_HASSIO_PATH}/scripts
 BOARD_DIR="$(dirname $0)"
 
-cp "$BOARD_DIR/rauc.conf" "$TARGET_DIR/etc/rauc/system.conf"
+. ${SCRIPT_DIR}/rootfs_layer.sh
+
+rootfs_fix
+
+cp ${BOARD_DIR}/rauc.conf ${TARGET_DIR}/etc/rauc/system.conf
