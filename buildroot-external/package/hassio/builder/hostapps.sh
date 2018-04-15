d69d7ce009b25fb61aca38f8dacaade6b52e3dcc
Pascal Vizeli <pascal.vizeli@syshack.ch>
2018-04-15 11:57:10 +0200

    Update hostapps.sh

--- buildroot-external/package/hassio/builder/hostapps.sh
+++ buildroot-external/package/hassio/builder/hostapps.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 set -e
+
+DATA_IMG="/export/data.img"
+
+# Make image
+dd if=/dev/zero of=${DATA_IMG} bs=1M count=1024
+mkfs.ext4 -E lazy_itable_init=0,lazy_journal_init=0 -i 8192 ${DATA_IMG}
+
+# Mount / init file structs
+mount -o loop ${DATA_IMG} /mnt
+mkdir -p /mnt/docker
+mkdir -p /mnt/supervisor
+mkdir -p /mnt/cli
