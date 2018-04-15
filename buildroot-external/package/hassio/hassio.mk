df5c9e5605e34af41e453dd546b37d4b5c99e1df
Pascal Vizeli <pvizeli@syshack.ch>
2018-04-15 10:59:55 +0200

    Init Builder scripts

---
+++ buildroot-external/package/hassio/hassio.mk
@@ -0,0 +1,9 @@
+################################################################################
+#
+# hassio
+#
+################################################################################
+
+HASSIO_VERSION = 0.1
+HASSIO_LICENSE = Apache License 2.0
+HASSIO_LICENSE_FILES = https://raw.githubusercontent.com/home-assistant/hassio-os/master/LICENSE
