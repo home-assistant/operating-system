f6fbdd4bb6a39d2dd94ac97085f1f83f0251d2ef
Pascal Vizeli <pascal.vizeli@syshack.ch>
2018-04-15 18:53:46 +0200

    Update post-image.sh

--- buildroot-external/board/ova/post-image.sh
+++ buildroot-external/board/ova/post-image.sh
@@ -3,27 +3,22 @@ set -e
 
 SCRIPT_DIR=${BR2_EXTERNAL_HASSIO_PATH}/scripts
 BOARD_DIR="$(dirname $0)"
-
-OVERLAY_IMG=${BINARIES_DIR}/overlay.ext4
-DATA_IMG=${BINARIES_DIR}/data.ext4
-BOOT_IMG=${BINARIES_DIR}/boot.vfat
-
-BOOT_DATA=${BINARIES_DIR}/boot-data
+BOOT_DATA=${BINARIES_DIR}/boot
 
 . ${SCRIPT_DIR}/hdd_image.sh
 
-rm -rf ${BOOT_DATA}
-
 # Init boot data
+rm -rf ${BOOT_DATA}
 mkdir -p ${BOOT_DATA}/EFI/BOOT
 mkdir -p ${BOOT_DATA}/EFI/barebox
 
 cp ${BINARIES_DIR}/barebox.bin ${BOOT_DATA}/EFI/BOOT/BOOTx64.EFI
 cp ${BOARD_DIR}/barebox-state.dtb ${BOOT_DATA}/EFI/barebox/state.dtb
 
-hassio_boot_image ${BOOT_DATA} ${BOOT_IMG}
-hassio_overlay_image ${OVERLAY_IMG}
+# Create other layers
+hassio_boot_image ${BINARIES_DIR}
+hassio_overlay_image ${BINARIES_DIR}
 
-hassio_hdd_image ${BOOT_IMG} ${BINARIES_DIR}/rootfs.squashfs ${OVERLAY_IMG} ${DATA_IMG} ${BINARIES_DIR}/harddisk.img
+hassio_hdd_image ${BINARIES_DIR} ${BINARIES_DIR}/harddisk.img
 
 qemu-img convert -O vmdk ${BINARIES_DIR}/harddisk.img ${BINARIES_DIR}/hassio-os.vmdk
