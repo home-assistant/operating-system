4d8015af46b70456c94a724d603f3a990c7317e9
Pascal Vizeli <pascal.vizeli@syshack.ch>
2018-04-29 00:40:06 +0200

    Update apparmor.mk

--- buildroot-external/package/apparmor/apparmor.mk
+++ buildroot-external/package/apparmor/apparmor.mk
@@ -6,7 +6,10 @@
 APPARMOR_VERSION = 2.13.0
 APPARMOR_SOURCE  = apparmor-2.13.tar.gz
 APPARMOR_SITE    = https://launchpad.net/apparmor/2.13/${APPARMOR_VERSION}/+download
-APPARMOR_CONF_OPTS = 
-APPARMOR_DEPENDENCIES = 
+APPARMOR_LICENSE = GPL-2
+APPARMOR_LICENSE_FILES = LICENSE
+APPARMOR_INSTALL_STAGING = YES
 
-$(eval $(autotools-package))
+APPARMOR_CONF_OPTS = --prefix=/usr
+
+$(eval $(generic-package))
