74d524e9b9abb4c87bb336cbde95521eeeb788f4
Pascal Vizeli <pvizeli@syshack.ch>
2018-04-29 12:53:40 +0200

    test 1

--- buildroot-external/package/apparmor/apparmor.mk
+++ buildroot-external/package/apparmor/apparmor.mk
@@ -7,28 +7,11 @@ APPARMOR_VERSION = v2.13
 APPARMOR_SITE = git://git.launchpad.net/apparmor
 APPARMOR_LICENSE = GPL-2
 APPARMOR_LICENSE_FILES = LICENSE
-APPARMOR_INSTALL_STAGING = YES
-APPARMOR_DEPENDENCIES = host-flex
-
-APPARMOR_CONF_ENV = ac_cv_func_reallocarray=no
-APPARMOR_CONF_OPTS = \
-	--prefix=/usr \
-	--disable-doc
-
-define APPARMOR_CONFIGURE_CMDS
-	cd $(@D)/libraries/libapparmor && \
-	PATH=$(BR_PATH) ./autogen.sh && \
-	PATH=$(BR_PATH) ./configure $(APPARMOR_CONF_OPTS)
-endef
+APPARMOR_DEPENDENCIES = libapparmor
 
 define APPARMOR_BUILD_CMDS
-	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)/libraries/libapparmor
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)/parser
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)/profiles
-endef
-
-define APPARMOR_INSTALL_STAGING_CMDS
-	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)/libraries/libapparmor DESTDIR=$(STAGING_DIR) PREFIX=/usr install
 endef
 
 define APPARMOR_INSTALL_TARGET_CMDS
