abe5680b979cdc695590cd5ce16189bcc01dac5c
Pascal Vizeli <pascal.vizeli@syshack.ch>
2018-04-29 11:10:40 +0200

    Update apparmor.mk

--- buildroot-external/package/apparmor/apparmor.mk
+++ buildroot-external/package/apparmor/apparmor.mk
@@ -13,22 +13,22 @@ APPARMOR_CONF_OPTS = \
 	--prefix=/usr
 
 define APPARMOR_BUILD_CMDS
-	cd libraries/libapparmor && \
+	cd $(@D)/libraries/libapparmor && \
 	./autogen.sh && \
 	./configure $(APPARMOR_CONF_OPTS)
 	
-	$(MAKE) $(STAGING_CONFIGURE_OPTS) -C libraries/libapparmor
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C parser
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C profile
+	$(MAKE) $(STAGING_CONFIGURE_OPTS) -C $(@D)/libraries/libapparmor
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/parser
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/profiles
 endef
 
 define APPARMOR_INSTALL_STAGING_CMDS
-	$(STAGING_MAKE_ENV) $(MAKE) -C DESTDIR=$(STAGING_DIR) libraries/libapparmor install
+	$(STAGING_MAKE_ENV) $(MAKE) -C $(@D)/libraries/libapparmor DESTDIR=$(STAGING_DIR) install
 endef
 
 define APPARMOR_INSTALL_TARGET_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) -C DESTDIR=$(TARGET_DIR) parser install
-	$(TARGET_MAKE_ENV) $(MAKE) -C DESTDIR=$(TARGET_DIR) profile install
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)/parser DESTDIR=$(TARGET_DIR) install
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)/profiles DESTDIR=$(TARGET_DIR) install
 endef
 
 $(eval $(generic-package))
