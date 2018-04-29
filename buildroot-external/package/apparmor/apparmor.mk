c5d73bde09f8e0d7db22316f78c3bd9dd7916ecb
Pascal Vizeli <pascal.vizeli@syshack.ch>
2018-04-29 10:35:28 +0200

    Update apparmor.mk

--- buildroot-external/package/apparmor/apparmor.mk
+++ buildroot-external/package/apparmor/apparmor.mk
@@ -3,13 +3,33 @@
 # apparmor
 #
 #############################################################
-APPARMOR_VERSION = 2.13.0
-APPARMOR_SOURCE  = apparmor-2.13.tar.gz
-APPARMOR_SITE    = https://launchpad.net/apparmor/2.13/${APPARMOR_VERSION}/+download
+APPARMOR_VERSION_MAJOR = 2.13
+APPARMOR_VERSION = $(APPARMOR_VERSION_MAJOR).0
+APPARMOR_SITE    = https://launchpad.net/apparmor/$(APPARMOR_VERSION_MAJOR)/$(APPARMOR_VERSION)/+download
 APPARMOR_LICENSE = GPL-2
 APPARMOR_LICENSE_FILES = LICENSE
 APPARMOR_INSTALL_STAGING = YES
 
-APPARMOR_CONF_OPTS = --prefix=/usr
+APPARMOR_CONF_OPTS = \
+	--prefix=/usr
+
+define APPARMOR_BUILD_CMDS
+	cd libraries/libapparmor && \
+	./autogen.sh && \
+	./configure $(APPARMOR_CONF_OPTS)
+	
+	$(MAKE) -C libraries/libapparmor
+	$(MAKE) -C parser
+	$(MAKE) -C profile
+endif
+
+define APPARMOR_STAGING_CMDS
+	$(MAKE) -C DESTDIR=$(STAGING_DIR) libraries/libapparmor install
+endif
+
+define APPARMOR_TARGET_CMDS
+	$(MAKE) -C DESTDIR=$(TARGET_DIR) parser install
+	$(MAKE) -C DESTDIR=$(TARGET_DIR) profile install
+endif
 
 $(eval $(generic-package))
