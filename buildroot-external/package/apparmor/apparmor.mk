1e62743b1e7ee908a259f2d61d29029e4c202ef6
Pascal Vizeli <pascal.vizeli@syshack.ch>
2018-04-29 14:01:07 +0200

    Apparmor (#9)
    * Create Config.in
    
    * Update Config.in
    
    * Create apparmor.mk
    
    * Update Config.in
    
    * Create apparmor.hash
    
    * Update and rename buildroot-external/package/apparmor/Config.in to buildroot-external/package/libapparmor/Config.in
    
    * Create Config.in
    
    * Delete Config.in
    
    * Update Config.in
    
    * Update Config.in
    
    * Update apparmor.mk
    
    * Update apparmor.mk
    
    * Update apparmor.mk
    
    * Update Config.in
    
    * Update apparmor.mk
    
    * Update apparmor.mk
    
    * Delete apparmor.hash
    
    * Update apparmor.mk
    
    * Update apparmor.mk
    
    * Update apparmor.mk
    
    * Update apparmor.mk
    
    * Update apparmor.mk
    
    * test 1
    
    * Fix stuff

---
+++ buildroot-external/package/apparmor/apparmor.mk
@@ -0,0 +1,22 @@
+#############################################################
+#
+# apparmor
+#
+#############################################################
+APPARMOR_VERSION = v2.13
+APPARMOR_SITE = git://git.launchpad.net/apparmor
+APPARMOR_LICENSE = GPL-2
+APPARMOR_LICENSE_FILES = LICENSE
+APPARMOR_DEPENDENCIES = libapparmor
+
+define APPARMOR_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) PATH=$(BR_PATH) $(MAKE) -C $(@D)/parser USE_SYSTEM=1 YACC=bison LEX=flex 
+	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)/profiles
+endef
+
+define APPARMOR_INSTALL_TARGET_CMDS
+	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)/parser DESTDIR=$(TARGET_DIR) USE_SYSTEM=1 PREFIX=/usr install
+	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)/profiles DESTDIR=$(TARGET_DIR) PREFIX=/usr install
+endef
+
+$(eval $(generic-package))
