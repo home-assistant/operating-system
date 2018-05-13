9f79e421c190a647f6c0d1dec5d00dbb147f5c62
Pascal Vizeli <pvizeli@syshack.ch>
2018-05-13 21:03:29 +0200

    Update ovf-create.sh

--- scripts/ovf-create.sh
+++ scripts/ovf-create.sh
@@ -6,4 +6,4 @@ VBoxManage modifyvm Hass.io --cpus 2 --memory 1048 --firmware efi
 VBoxManage modifyvm Hass.io --nic1 bridged
 VBoxManage storageattach Hass.io --storagectl "SATA Controller" --device 0 --port 0 --type vmdk --medium $1
 
-VBoxManage export Hass.io --ovf20 --vendor "Home-Assistant" --vendorurl "http://hass.io" --output $2
+VBoxManage export Hass.io --ovf20 --vendor "Home Assistant" --vendorurl "http://hass.io" --output $2
