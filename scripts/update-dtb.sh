d784ba8c365d45ab3b34d037eac0bb87639b19ce
Pascal Vizeli <pvizeli@syshack.ch>
2018-05-27 11:43:24 +0000

    Add overlay for state to rpi

--- scripts/update-dtb.sh
+++ scripts/update-dtb.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-dtc -O dtb -o buildroot-external/fdt/barebox-state-efi.dtb buildroot-external/fdt/barebox-state-efi.dts
+dtc -I dts -O dtb -o buildroot-external/fdt/barebox-state-efi.dtb buildroot-external/fdt/barebox-state-efi.dts
+
+dtc -@ -I dts -O dtb -o buildroot-external/fdt/barebox-state-rpi.dtbo buildroot-external/fdt/barebox-state-rpi.dtso
