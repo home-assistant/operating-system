4203169741dc92c328ae8c63abd539c1ccdf60b2
Pascal Vizeli <pvizeli@syshack.ch>
2018-05-27 22:00:10 +0000

    Fix

---
+++ buildroot/package/docker-proxy/docker-proxy.mk
@@ -0,0 +1,23 @@
+################################################################################
+#
+# docker-proxy
+#
+################################################################################
+
+DOCKER_PROXY_VERSION = v0.7.2-rc.1
+DOCKER_PROXY_SITE = $(call github,docker,libnetwork,$(DOCKER_PROXY_VERSION))
+
+DOCKER_PROXY_LICENSE = Apache-2.0
+DOCKER_PROXY_LICENSE_FILES = LICENSE
+
+DOCKER_PROXY_DEPENDENCIES = host-pkgconf
+
+DOCKER_PROXY_WORKSPACE = gopath
+
+DOCKER_PROXY_BUILD_TARGETS = cmd/proxy
+
+define DOCKER_PROXY_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/bin/proxy $(TARGET_DIR)/usr/bin/docker-proxy
+endef
+
+$(eval $(golang-package))
