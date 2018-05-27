b58a1a1769e0ebe5e3de37638ecef7bef82d3950
Pascal Vizeli <pvizeli@syshack.ch>
2018-05-27 13:22:08 +0200

    Update update-dtb.sh

--- scripts/update-dtb.sh
+++ scripts/update-dtb.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-dtc -O dtb -o buildroot-external/fdt/barebox-state-efi.dtb buildroot-external/board/fdt/barebox-state-efi.dts
+dtc -O dtb -o buildroot-external/fdt/barebox-state-efi.dtb buildroot-external/fdt/barebox-state-efi.dts
