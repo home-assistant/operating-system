38c7ff48a82cc9ad5c759f5b3a79ee571d319e3c
Pascal Vizeli <pvizeli@syshack.ch>
2018-05-28 12:49:18 +0000

    fix versions

--- buildroot/package/docker-containerd/docker-containerd.mk
+++ buildroot/package/docker-containerd/docker-containerd.mk
@@ -5,23 +5,18 @@
 ################################################################################
 
 DOCKER_CONTAINERD_VERSION = v1.1.0
-DOCKER_CONTAINERD_COMMIT = 209a7fc3e4a32ef71a8c7b50c68fc8398415badf
 DOCKER_CONTAINERD_SITE = $(call github,containerd,containerd,$(DOCKER_CONTAINERD_VERSION))
 DOCKER_CONTAINERD_LICENSE = Apache-2.0
 DOCKER_CONTAINERD_LICENSE_FILES = LICENSE.code
 
-DOCKER_CONTAINERD_WORKSPACE = vendor
-
-DOCKER_CONTAINERD_LDFLAGS = \
-	-X github.com/containerd/containerd.GitCommit=$(DOCKER_CONTAINERD_COMMIT)
-
-DOCKER_CONTAINERD_BUILD_TARGETS = ctr containerd containerd-shim
+DOCKER_CONTAINERD_BUILD_TARGETS = cmd/ctr cmd/containerd cmd/containerd-shim
 DOCKER_CONTAINERD_TAGS = apparmor no_btrfs
 
 DOCKER_CONTAINERD_INSTALL_BINS = containerd containerd-shim
 
 define DOCKER_CONTAINERD_INSTALL_SYMLINKS
 	ln -fs runc $(TARGET_DIR)/usr/bin/docker-runc
+	ln -fs containerd $(TARGET_DIR)/usr/bin/docker-containerd
 	ln -fs containerd-shim $(TARGET_DIR)/usr/bin/docker-containerd-shim
 endef
 
--- buildroot/package/docker-proxy/docker-proxy.mk
+++ buildroot/package/docker-proxy/docker-proxy.mk
@@ -4,15 +4,13 @@
 #
 ################################################################################
 
-DOCKER_PROXY_VERSION = v0.7.2-rc.1
+DOCKER_PROXY_VERSION = 8436c5cdbc627b9c51f9a02742c5a3955b19c5e1
 DOCKER_PROXY_SITE = $(call github,docker,libnetwork,$(DOCKER_PROXY_VERSION))
 
 DOCKER_PROXY_LICENSE = Apache-2.0
 DOCKER_PROXY_LICENSE_FILES = LICENSE
 
 DOCKER_PROXY_DEPENDENCIES = host-pkgconf
-
-DOCKER_PROXY_WORKSPACE = gopath
 
 DOCKER_PROXY_BUILD_TARGETS = cmd/proxy
 
