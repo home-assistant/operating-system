9ec32eda850fc377a0b37c631fd7ce197d153538
Pascal Vizeli <pvizeli@syshack.ch>
2018-06-03 22:15:41 +0000

    Rpi upstream overlay

--- scripts/update-dtb.sh
+++ scripts/update-dtb.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
-dtc -I dts -O dtb -o buildroot-external/fdt/barebox-state-efi.dtb buildroot-external/fdt/barebox-state-efi.dts
+## OVA
+dtc -@ -I dts -O dtb -o fdt/barebox-state-efi.dtb fdt/barebox-state-efi.dts
+cp -f fdt/barebox-state-efi.dtb buildroot-external/board/ova/
 
-dtc -@ -I dts -O dtb -o buildroot-external/fdt/barebox-state-rpi.dtbo buildroot-external/fdt/barebox-state-rpi.dtso
+## Raspberry
+dtc -@ -I dts -O dtb -o fdt/barebox-state-rpi.dtbo fdt/barebox-state-rpi.dtso
+cp -f fdt/barebox-state-rpi.dtbo  buildroot-external/board/rpi2/barebox-env/overlay/
+
+dtc -@ -I dts -O dtb -o fdt/rpi-v3d.dtbo fdt/rpi-v3d.dtso
+cp -f fdt/rpi-v3d.dtbo  buildroot-external/board/rpi2/barebox-env/overlay/
+
+dtc -@ -I dts -O dtb -o fdt/rpi-dwc.dtbo fdt/rpi-dwc.dtso
+cp -f fdt/rpi-dwc.dtbo  buildroot-external/board/rpi2/barebox-env/overlay/
