2afef71117c04fa17257915dc2c5bb1febd133fb
Pascal Vizeli <pvizeli@syshack.ch>
2018-06-04 16:30:01 +0000

    Fix overlay sdhost

--- scripts/update-dtb.sh
+++ scripts/update-dtb.sh
@@ -9,7 +9,8 @@ dtc -@ -I dts -O dtb -o fdt/barebox-state-rpi.dtbo fdt/barebox-state-rpi.dtso
 cp -f fdt/barebox-state-rpi.dtbo  buildroot-external/board/rpi2/barebox-env/overlay/
 
 dtc -@ -I dts -O dtb -o fdt/rpi-v3d.dtbo fdt/rpi-v3d.dtso
-cp -f fdt/rpi-v3d.dtbo  buildroot-external/board/rpi2/barebox-env/overlay/
+#cp -f fdt/rpi-v3d.dtbo  buildroot-external/board/rpi2/barebox-env/overlay/
 
 dtc -@ -I dts -O dtb -o fdt/rpi-dwc.dtbo fdt/rpi-dwc.dtso
-cp -f fdt/rpi-dwc.dtbo  buildroot-external/board/rpi2/barebox-env/overlay/
+#cp -f fdt/rpi-dwc.dtbo  buildroot-external/board/rpi2/barebox-env/overlay/
+
