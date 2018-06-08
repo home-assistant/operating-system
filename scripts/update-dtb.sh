a57864c199dfed9325806302c884f60641ade8b8
Pascal Vizeli <pvizeli@syshack.ch>
2018-06-08 13:00:18 +0000

    Better group boards like rpi
    Signed-off-by: Pascal Vizeli <pvizeli@syshack.ch>

--- scripts/update-dtb.sh
+++ scripts/update-dtb.sh
@@ -6,5 +6,5 @@ cp -f fdt/barebox-state-efi.dtb buildroot-external/board/ova/
 
 ## Raspberry
 dtc -@ -I dts -O dtb -o fdt/barebox-state-rpi.dtbo fdt/barebox-state-rpi.dtso
-cp -f fdt/barebox-state-rpi.dtbo  buildroot-external/board/rpi2/barebox-env/overlay/
+cp -f fdt/barebox-state-rpi.dtbo  buildroot-external/board/raspberrypi/barebox-env/data/
 
