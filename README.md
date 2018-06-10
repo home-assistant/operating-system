2cd1bbbe3767bf38908db32cd4acc284892450e2
Pascal Vizeli <pvizeli@syshack.ch>
2018-06-10 09:11:05 +0000

    Support dual bootloader
    Signed-off-by: Pascal Vizeli <pvizeli@syshack.ch>

--- README.md
+++ README.md
@@ -4,10 +4,11 @@ Hass.io OS based on [buildroot](https://buildroot.org/). It's a hypervisor for D
 ## Focus
 
 - Linux kernel 4.14 (LT)
-- Barebox as bootloader
+- Barebox as bootloader on EFI
+- U-Boot as bootloader on IoT
 - RAUC for OTA updates
 - SquashFS LZ4 as filesystem
-- Docker 17.12.1
+- Docker 18.03.1
 - AppArmor protected
 - ZRAM LZ4 for /tmp, /var, swap
 - Run every supervisor
--- scripts/update-dtb.sh
+++ scripts/update-dtb.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
 ## OVA
-dtc -@ -I dts -O dtb -o fdt/barebox-state-efi.dtb fdt/barebox-state-efi.dts
-cp -f fdt/barebox-state-efi.dtb buildroot-external/board/ova/
-
-## Raspberry
-dtc -@ -I dts -O dtb -o fdt/barebox-state-rpi.dtbo fdt/barebox-state-rpi.dtso
-cp -f fdt/barebox-state-rpi.dtbo  buildroot-external/board/raspberrypi/barebox-env/data/
+dtc -@ -I dts -O dtb -o buildroot-external/misc/barebox-state-efi.dtb buildroot-external/misc/barebox-state-efi.dts
 
