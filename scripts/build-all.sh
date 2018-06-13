eae36599094a476a5223b47ba630eee5dd8a7301
Pascal Vizeli <pvizeli@syshack.ch>
2018-06-13 19:28:05 +0000

    Add Builder script
    Signed-off-by: Pascal Vizeli <pvizeli@syshack.ch>

---
+++ scripts/build-all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+mkdir -p /build/RL
+
+all_platforms=(ova rpi rpi0_w rpi2 rpi3 rpi3_64)
+for platform in "${all_platforms[@]}"; do
+    make -C /build/buildroot BR2_EXTERNAL=/build/buildroot-external \
+        ${platform}_defconfig
+    make -C /build/buildroot BR2_EXTERNAL=/build/buildroot-external \
+        make
+    make -C /build/buildroot BR2_EXTERNAL=/build/buildroot-external \
+        clean
+
+    cp -f /build/buildroot/output/images/hassos_* /build/RL/
+done
