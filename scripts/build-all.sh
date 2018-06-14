95f82aa75d17e1b356cae32cc0542aac5ef35ab5
Pascal Vizeli <pvizeli@syshack.ch>
2018-06-14 05:15:10 +0000

    Fix build script
    Signed-off-by: Pascal Vizeli <pvizeli@syshack.ch>

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -7,8 +7,7 @@ all_platforms=(ova rpi rpi0_w rpi2 rpi3 rpi3_64)
 for platform in "${all_platforms[@]}"; do
     make -C /build/buildroot BR2_EXTERNAL=/build/buildroot-external \
         ${platform}_defconfig
-    make -C /build/buildroot BR2_EXTERNAL=/build/buildroot-external \
-        make
+    make -C /build/buildroot BR2_EXTERNAL=/build/buildroot-external
     make -C /build/buildroot BR2_EXTERNAL=/build/buildroot-external \
         clean
 
