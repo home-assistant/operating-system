dd6d8d7144c65ed6c2cb9265d6aa01a79a538878
Pascal Vizeli <pvizeli@syshack.ch>
2018-06-21 17:06:36 +0200

    Update ota.sh

--- buildroot-external/scripts/ota.sh
+++ buildroot-external/scripts/ota.sh
@@ -2,7 +2,14 @@
 
 function create_ota_update() {
     local rauc_folder="${BINARIES_DIR}/rauc"
+    local boot_folder="${BINARIES_DIR}/boot"
+    local kernel="${BINARIES_DIR}/${2}"
+    local rootfs="${BINARIES_DIR}/rootfs.squash"
     
     rm -rf ${rauc_folder}
     mkdir -p ${rauc_folder}
+    
+    tar -pcf ${rauc_folder}/kernel.tar ${kernel}
+    tar -pcf ${rauc_folder}/boot.tar ${boot_folder}
+    cp -f ${rootfs} ${rauc_folder}/rootfs.img
 }
