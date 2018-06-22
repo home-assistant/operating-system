fa71f5264448f1f43ba71371b866e9aada83d31a
Pascal Vizeli <pvizeli@syshack.ch>
2018-06-22 13:27:51 +0200

    Update ota.sh

--- buildroot-external/scripts/ota.sh
+++ buildroot-external/scripts/ota.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 function create_ota_update() {
-    local ota_file="${BINARIES_DIR}/${1}.raucb"
+    local ota_file="${1}"
     local rauc_folder="${BINARIES_DIR}/rauc"
     local boot_folder="${BINARIES_DIR}/boot"
     local kernel="${BINARIES_DIR}/${2}"
     local rootfs="${BINARIES_DIR}/rootfs.squash"
     local key="/build/secure/key.pem"
     local cert="/build/secure/cert.pem"
-    
+
     rm -rf ${rauc_folder}
     mkdir -p ${rauc_folder}
-    
+
     tar -pcf ${rauc_folder}/kernel.tar ${kernel}
     tar -pcf ${rauc_folder}/boot.tar ${boot_folder}
     cp -f ${rootfs} ${rauc_folder}/rootfs.img
