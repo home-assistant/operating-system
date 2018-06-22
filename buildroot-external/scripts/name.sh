e82bce27a08bcd0fddda44018d38624309d264e4
Pascal Vizeli <pvizeli@syshack.ch>
2018-06-22 13:17:42 +0200

    Update name.sh

--- buildroot-external/scripts/name.sh
+++ buildroot-external/scripts/name.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 function hassos_image_name() {
-    echo ${HASSOS_ID}_${BOARD_ID}-${VERSION_MAJOR}.${VERSION_BUILD}
+    echo "${HASSOS_ID}_${BOARD_ID}-${VERSION_MAJOR}.${VERSION_BUILD}"
+}
+
+function hassos_rauc_compatible() {
+    echo "${HASSOS_ID}-${BOARD_ID}"
 }
