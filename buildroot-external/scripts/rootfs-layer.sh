a2d61c9b4a951ca55162165f91922210e350acf6
Pascal Vizeli <pvizeli@syshack.ch>
2018-07-04 22:31:21 +0000

    Store networkmanager

--- buildroot-external/scripts/rootfs-layer.sh
+++ buildroot-external/scripts/rootfs-layer.sh
@@ -2,10 +2,6 @@
 
 function fix_rootfs() {
 
-    # Cleanup DHCP service, we don't need this with NetworkManager
-    rm -rf ${TARGET_DIR}/etc/systemd/system/multi-user.target.wants/dhcpcd.service
-    rm -rf ${TARGET_DIR}/usr/lib/systemd/system/dhcpcd.service
-
     # Cleanup etc
     rm -rf ${TARGET_DIR}/etc/init.d
     rm -rf ${TARGET_DIR}/etc/network
