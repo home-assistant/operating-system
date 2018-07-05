6c58eb16150ee6eb349b6f7ed8463283c36b2688
Pascal Vizeli <pascal.vizeli@syshack.ch>
2018-07-05 13:44:52 +0200

    Fix /usr/sbin/hassos-cli is not in /etc/shells (#78)
    * Update rootfs-layer.sh
    
    * Update rootfs-layer.sh
    
    * Update rootfs-layer.sh

--- buildroot-external/scripts/rootfs-layer.sh
+++ buildroot-external/scripts/rootfs-layer.sh
@@ -31,4 +31,8 @@ function fix_rootfs() {
 function install_hassos_cli() {
 
     sed -i "s|\(root.*\)/bin/sh|\1/usr/sbin/hassos-cli|" ${TARGET_DIR}/etc/passwd
+    
+    if ! grep "hassos-cli" ${TARGET_DIR}/etc/shells; then
+        echo "/usr/sbin/hassos-cli" >> ${TARGET_DIR}/etc/shells
+    fi
 }
