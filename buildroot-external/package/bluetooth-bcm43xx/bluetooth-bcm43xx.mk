9ba40d25acd583aa397d1c97fc3e8f83e0d75c88
Pascal Vizeli <pvizeli@syshack.ch>
2018-07-09 20:36:49 +0000

    Auto enable

--- buildroot-external/package/bluetooth-bcm43xx/bluetooth-bcm43xx.mk
+++ buildroot-external/package/bluetooth-bcm43xx/bluetooth-bcm43xx.mk
@@ -11,6 +11,7 @@ BLUETOOTH_BCM43XX_SITE = $(BR2_EXTERNAL_HASSOS_PATH)/package/bluetooth-bcm43xx
 BLUETOOTH_BCM43XX_SITE_METHOD = local
 
 define BLUETOOTH_BCM43XX_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/etc/systemd/system/hassos-hardware.wants
 	cp -f $(BR2_EXTERNAL_HASSOS_PATH)/package/bluetooth-bcm43xx/bluetooth-bcm43xx.service $(TARGET_DIR)/usr/lib/systemd/system/bluetooth-bcm43xx.service
 	ln -fs /usr/lib/systemd/system/bluetooth-bcm43xx.service $(TARGET_DIR)/etc/systemd/system/hassos-hardware.wants/
 endef
