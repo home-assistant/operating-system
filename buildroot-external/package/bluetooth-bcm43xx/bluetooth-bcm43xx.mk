1c3b4818f3ea05d73c7c82076c4ec1095716ed8c
Pascal Vizeli <pvizeli@syshack.ch>
2018-07-09 20:29:56 +0000

    fix package

--- buildroot-external/package/bluetooth-bcm43xx/bluetooth-bcm43xx.mk
+++ buildroot-external/package/bluetooth-bcm43xx/bluetooth-bcm43xx.mk
@@ -1,16 +1,16 @@
 ################################################################################
 #
-# HassOS
+# Bluetooth BCM43xx
 #
 ################################################################################
 
-HASSOS_VERSION = 1.0.0
-HASSOS_LICENSE = Apache License 2.0
-HASSOS_LICENSE_FILES = $(BR2_EXTERNAL_HASSOS_PATH)/../LICENSE
-HASSOS_SITE = $(BR2_EXTERNAL_HASSOS_PATH)/package/bluetooth-bcm43xx
-HASSOS_SITE_METHOD = local
+BLUETOOTH_BCM43XX_VERSION = 1.0.0
+BLUETOOTH_BCM43XX_LICENSE = Apache License 2.0
+BLUETOOTH_BCM43XX_LICENSE_FILES = $(BR2_EXTERNAL_HASSOS_PATH)/../LICENSE
+BLUETOOTH_BCM43XX_SITE = $(BR2_EXTERNAL_HASSOS_PATH)/package/bluetooth-bcm43xx
+BLUETOOTH_BCM43XX_SITE_METHOD = local
 
-define HASSOS_INSTALL_TARGET_CMDS
+define BLUETOOTH_BCM43XX_INSTALL_TARGET_CMDS
 	cp -f $(BR2_EXTERNAL_HASSOS_PATH)/package/bluetooth-bcm43xx/bluetooth-bcm43xx.service $(TARGET_DIR)/usr/lib/systemd/system/bluetooth-bcm43xx.service
 	ln -fs /usr/lib/systemd/system/bluetooth-bcm43xx.service $(TARGET_DIR)/etc/systemd/system/hassos-hardware.wants/
 endef
