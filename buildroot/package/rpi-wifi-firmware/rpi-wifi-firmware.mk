9113d711f2770f2c83c3610334fd5b21e9322176
Pascal Vizeli <pascal.vizeli@syshack.ch>
2018-07-13 09:52:45 +0200

    Update rpi kernel 4.14.54 / Firmware / wifi (#98)
    * Update rpi kernel 4.14.54 / Firmware / wifi
    
    Signed-off-by: Pascal Vizeli <pvizeli@syshack.ch>
    
    * Fix patch
    
    * Update bluetooth-bcm43xx.mk
    
    * Update bluetooth-bcm43xx.mk
    
    * Update bluetooth-bcm43xx.mk

--- buildroot/package/rpi-wifi-firmware/rpi-wifi-firmware.mk
+++ buildroot/package/rpi-wifi-firmware/rpi-wifi-firmware.mk
@@ -4,14 +4,14 @@
 #
 ################################################################################
 
-RPI_WIFI_FIRMWARE_VERSION = 18d7c931aff0a8a78360b9b9eaeb15d1224fb907
-RPI_WIFI_FIRMWARE_SITE = $(call github,LibreELEC,brcmfmac_sdio-firmware-rpi,$(RPI_WIFI_FIRMWARE_VERSION))
+RPI_WIFI_FIRMWARE_VERSION = 86e88fbf0345da49555d0ec34c80b4fbae7d0cd3
+RPI_WIFI_FIRMWARE_SITE = $(call github,RPi-Distro,firmware-nonfree,$(RPI_WIFI_FIRMWARE_VERSION))
 RPI_WIFI_FIRMWARE_LICENSE = PROPRIETARY
 RPI_WIFI_FIRMWARE_LICENSE_FILES = LICENCE.broadcom_bcm43xx
 
 define RPI_WIFI_FIRMWARE_INSTALL_TARGET_CMDS
 	$(INSTALL) -d $(TARGET_DIR)/lib/firmware/brcm
-	$(INSTALL) -m 0644 $(@D)/firmware/brcm/brcmfmac* $(TARGET_DIR)/lib/firmware/brcm
+	$(INSTALL) -m 0644 $(@D)/brcm/brcmfmac434* $(TARGET_DIR)/lib/firmware/brcm
 endef
 
 $(eval $(generic-package))
