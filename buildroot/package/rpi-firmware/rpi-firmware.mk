8773ba44f3170c36d4d78cf8f8708584c5d1f123
Pascal Vizeli <pvizeli@syshack.ch>
2018-07-28 22:09:19 +0000

    rpi: Update kernel & firmware 4.14.58

--- buildroot/package/rpi-firmware/rpi-firmware.mk
+++ buildroot/package/rpi-firmware/rpi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_FIRMWARE_VERSION = 418b77e24476a98aba4ee33942cc5321e6698ce4
+RPI_FIRMWARE_VERSION = bffe7ee6b276f23822883f3d7fd7b705d12c26b9
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3-Clause
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
