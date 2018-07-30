b4eadf1566cbf2693a67abcb9448fa3198db86aa
Pascal Vizeli <pvizeli@syshack.ch>
2018-07-30 23:41:46 +0000

    Bump build u-boot

--- buildroot-external/board/tinker/uboot-boot.sh
+++ buildroot-external/board/tinker/uboot-boot.sh
@@ -10,7 +10,7 @@ setenv bootargs_a "root=PARTUUID=8d3d53e3-6d49-4c38-8349-aff6859e82fd rootfstype
 setenv bootargs_b "root=PARTUUID=a3ec664e-32ce-4665-95ea-7ae90ce9aa20 rootfstype=squashfs ro"
 
 # Load device tree
-fatload mmc 0:1 ${fdt_addr_r} rk3288-tinker.dtb
+fatload mmc 1:1 ${fdt_addr_r} rk3288-tinker.dtb
 
 setenv bootargs
 for BOOT_SLOT in "${BOOT_ORDER}"; do
@@ -20,14 +20,14 @@ for BOOT_SLOT in "${BOOT_ORDER}"; do
     if test ${BOOT_A_LEFT} -gt 0; then
       setexpr BOOT_A_LEFT ${BOOT_A_LEFT} - 1
       echo "Found valid slot A, ${BOOT_A_LEFT} attempts remaining"
-      setenv load_kernel "ext4load mmc 0:2 ${kernel_addr_r} zImage"
+      setenv load_kernel "ext4load mmc 1:2 ${kernel_addr_r} zImage"
       setenv bootargs "${bootargs_hassos} ${bootargs_a} rauc.slot=A"
     fi
   elif test "x${BOOT_SLOT}" = "xB"; then
     if test ${BOOT_B_LEFT} -gt 0; then
       setexpr BOOT_B_LEFT ${BOOT_B_LEFT} - 1
       echo "Found valid slot B, ${BOOT_B_LEFT} attempts remaining"
-      setenv load_kernel "ext4load mmc 0:4 ${kernel_addr_r} zImage"
+      setenv load_kernel "ext4load mmc 1:4 ${kernel_addr_r} zImage"
       setenv bootargs "${bootargs_hassos} ${bootargs_b} rauc.slot=B"
     fi
   fi
