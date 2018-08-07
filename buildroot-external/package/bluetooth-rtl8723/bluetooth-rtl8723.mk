3a42ad1a398b873d4d62d27c9c166e0ec6860414
Pascal Vizeli <pvizeli@syshack.ch>
2018-08-07 14:44:28 +0000

    Fix bl

--- buildroot-external/package/bluetooth-rtl8723/bluetooth-rtl8723.mk
+++ buildroot-external/package/bluetooth-rtl8723/bluetooth-rtl8723.mk
@@ -13,8 +13,8 @@ BLUETOOTH_RTL8723_SITE_METHOD = local
 define BLUETOOTH_RTL8723_BUILD_CMDS
 	curl -L -o $(@D)/rtk_hciattach https://raw.githubusercontent.com/armbian/build/dee62df8bb2fe8611fd41ddf02063fa15533298c/packages/bsp/rockchip/rtk_hciattach
 
-	curl -L -o $(@D)/rtl8723b_config.bin https://raw.githubusercontent.com/armbian/firmware/4723bbb3d1ef70b5fbe7d2599c47d078ab125c47/rtl_bt/rtl8723b_config.bin
-	curl -L -o $(@D)/rtl8723b_fw.bin https://raw.githubusercontent.com/armbian/firmware/4723bbb3d1ef70b5fbe7d2599c47d078ab125c47/rtl_bt/rtl8723b_fw.bin
+	curl -L -o $(@D)/rtl8723b_config https://raw.githubusercontent.com/armbian/firmware/4723bbb3d1ef70b5fbe7d2599c47d078ab125c47/rtl_bt/rtl8723b_config.bin
+	curl -L -o $(@D)/rtl8723b_fw https://raw.githubusercontent.com/armbian/firmware/4723bbb3d1ef70b5fbe7d2599c47d078ab125c47/rtl_bt/rtl8723b_fw.bin
 endef
 
 define BLUETOOTH_RTL8723_INSTALL_TARGET_CMDS
@@ -24,7 +24,7 @@ define BLUETOOTH_RTL8723_INSTALL_TARGET_CMDS
 	ln -fs /usr/lib/systemd/system/bluetooth-rtl8723.service $(TARGET_DIR)/etc/systemd/system/hassos-hardware.target.wants/
 
 	$(INSTALL) -d $(TARGET_DIR)/lib/firmware/rtlbt
-	$(INSTALL) -m 0644 $(@D)/*.bin $(TARGET_DIR)/lib/firmware/rtlbt/
+	$(INSTALL) -m 0644 $(@D)/rtl8723b_* $(TARGET_DIR)/lib/firmware/rtlbt/
 	$(INSTALL) -m 0755 $(@D)/rtk_hciattach $(TARGET_DIR)/usr/sbin/
 endef
 
