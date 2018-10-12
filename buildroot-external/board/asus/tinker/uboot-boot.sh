5bfaf4bc5693dc2b29eeddb699f0250102c69b83
Ryan Bray <rbray89@gmail.com>
2018-10-11 21:39:26 -0600

    Tinker: Add potential eMMC boot support.

--- buildroot-external/board/asus/tinker/uboot-boot.sh
+++ buildroot-external/board/asus/tinker/uboot-boot.sh
@@ -24,10 +24,10 @@ setenv bootargs_a "root=PARTUUID=8d3d53e3-6d49-4c38-8349-aff6859e82fd rootfstype
 setenv bootargs_b "root=PARTUUID=a3ec664e-32ce-4665-95ea-7ae90ce9aa20 rootfstype=squashfs ro"
 
 # Load extraargs
-fileenv mmc 1:1 ${ramdisk_addr_r} cmdline.txt cmdline
+fileenv mmc ${devnum}:1 ${ramdisk_addr_r} cmdline.txt cmdline
 
 # Load device tree
-fatload mmc 1:1 ${fdt_addr_r} rk3288-tinker.dtb
+fatload mmc ${devnum}:1 ${fdt_addr_r} rk3288-tinker.dtb
 
 setenv bootargs
 for BOOT_SLOT in "${BOOT_ORDER}"; do
@@ -37,14 +37,14 @@ for BOOT_SLOT in "${BOOT_ORDER}"; do
     if test ${BOOT_A_LEFT} -gt 0; then
       setexpr BOOT_A_LEFT ${BOOT_A_LEFT} - 1
       echo "Found valid slot A, ${BOOT_A_LEFT} attempts remaining"
-      setenv load_kernel "ext4load mmc 1:2 ${kernel_addr_r} zImage"
+      setenv load_kernel "ext4load mmc ${devnum}:2 ${kernel_addr_r} zImage"
       setenv bootargs "${bootargs_hassos} ${bootargs_a} rauc.slot=A ${cmdline}"
     fi
   elif test "x${BOOT_SLOT}" = "xB"; then
     if test ${BOOT_B_LEFT} -gt 0; then
       setexpr BOOT_B_LEFT ${BOOT_B_LEFT} - 1
       echo "Found valid slot B, ${BOOT_B_LEFT} attempts remaining"
-      setenv load_kernel "ext4load mmc 1:4 ${kernel_addr_r} zImage"
+      setenv load_kernel "ext4load mmc ${devnum}:4 ${kernel_addr_r} zImage"
       setenv bootargs "${bootargs_hassos} ${bootargs_b} rauc.slot=B ${cmdline}"
     fi
   fi
