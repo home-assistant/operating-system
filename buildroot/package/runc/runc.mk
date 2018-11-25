f5442f944757dec417e210be442bf96cea15b153
Pascal Vizeli <pvizeli@syshack.ch>
2018-11-25 16:01:45 +0000

    runc update

--- buildroot/package/runc/runc.mk
+++ buildroot/package/runc/runc.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 # docker-engine/hack/dockerfile/install/runc.installer:4 RUNC_COMMIT=...
-RUNC_VERSION = 69663f0bd4b60df09991c08812a60108003fa340
+RUNC_VERSION = v1.0.0-rc6
 RUNC_SITE = $(call github,opencontainers,runc,$(RUNC_VERSION))
 RUNC_LICENSE = Apache-2.0
 RUNC_LICENSE_FILES = LICENSE
@@ -14,7 +14,7 @@ RUNC_WORKSPACE = Godeps/_workspace
 
 RUNC_LDFLAGS = -X main.gitCommit=$(RUNC_VERSION)
 
-RUNC_TAGS = cgo static_build
+RUNC_TAGS = cgo static_build apparmor
 
 ifeq ($(BR2_PACKAGE_LIBSECCOMP),y)
 RUNC_TAGS += seccomp
