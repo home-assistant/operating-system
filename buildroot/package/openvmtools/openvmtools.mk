32c8d4acbb778e7f09bdd820d1e96794a561389a
Pascal Vizeli <pvizeli@syshack.ch>
2018-12-03 16:10:49 +0000

    Fix openvmtools

--- buildroot/package/openvmtools/openvmtools.mk
+++ buildroot/package/openvmtools/openvmtools.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OPENVMTOOLS_VERSION = 5a9033ddfa95786d867e4d02bbb9a29bac8fb64f
+OPENVMTOOLS_VERSION = f2ff192717375b95a6b7e278fb47dbb3d3bc56d1
 OPENVMTOOLS_SITE = $(call github,vmware,open-vm-tools,$(OPENVMTOOLS_VERSION))
 OPENVMTOOLS_SUBDIR = open-vm-tools
 OPENVMTOOLS_LICENSE = LGPL-2.1
