e8d14bdc608e352b69d10c8a1b0ef817a39a1139
Pascal Vizeli <pascal.vizeli@syshack.ch>
2019-05-14 01:16:40 +0200

    Support build as user (#405)
    * Support build as user
    
    * Update enter.sh
    
    * Update Dockerfile
    
    * Update enter.sh
    
    * Update entry.sh

--- scripts/enter.sh
+++ scripts/enter.sh
@@ -2,4 +2,7 @@
 modprobe overlayfs
 
 docker build -t hassbuildroot .
-docker run -it --rm --privileged -v "$(pwd):/build" -v "${CACHE_DIR:=$HOME/hassos-cache}:/cache" hassbuildroot bash
+docker run -it --rm --privileged \
+  -v "$(pwd):/build" -v "${CACHE_DIR:=$HOME/hassos-cache}:/cache" \
+  -e BUILDER_UID="$(id -u)" -e BUILDER_GID="$(id -g)" \
+  hassbuildroot bash
--- scripts/entry.sh
+++ scripts/entry.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 set -e
 
+USER="root"
+
 # Run dockerd
 dockerd -s vfs &> /dev/null &
 
+# Setup local user
+if [ "${BUILDER_UID:0}" -ne 0 ] && [ "${BUILDER_GID:0}" -ne 0 ]; then
+  groupadd -g "${BUILDER_GID}" builder
+  useradd -m -u "${BUILDER_UID}" -g "${BUILDER_GID}" -G docker builder
+  USER="builder"
+fi
+
 if CMD="$(command -v "$1")"; then
   shift
-  exec "$CMD" "$@"
+  sudo -H -u ${USER} "$CMD" "$@"
 else
   echo "Command not found: $1"
   exit 1
