02da305aecb127ca2e700313883ab82f474c0166
Pascal Vizeli <pvizeli@syshack.ch>
2019-12-10 09:11:58 +0000

    Add to buildroot

--- buildroot/package/rpi-wifi-firmware/rpi-wifi-firmware.mk
+++ buildroot/package/rpi-wifi-firmware/rpi-wifi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_WIFI_FIRMWARE_VERSION = 130cb86fa30cafbd575d38865fa546350d4c5f9c
+RPI_WIFI_FIRMWARE_VERSION = 00daf85ffa373ecce7836df7543c6ebe4cf43639
 RPI_WIFI_FIRMWARE_SITE = $(call github,RPi-Distro,firmware-nonfree,$(RPI_WIFI_FIRMWARE_VERSION))
 RPI_WIFI_FIRMWARE_LICENSE = PROPRIETARY
 RPI_WIFI_FIRMWARE_LICENSE_FILES = LICENCE.broadcom_bcm43xx
