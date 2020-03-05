a4237dabfbd231a866af3707780db9178d7920e0
Pascal Vizeli <pvizeli@syshack.ch>
2020-03-05 15:32:19 +0000

    Update RaspberryPi kernel 4.19.106

--- buildroot/package/rpi-firmware/rpi-firmware.mk
+++ buildroot/package/rpi-firmware/rpi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_FIRMWARE_VERSION = 62a0d75b67484cb306a2a6f8fa954d9644868169
+RPI_FIRMWARE_VERSION = bf83b506b4a6f9e592e711d02871a278fad5bd1e
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3-Clause
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
