34b7bbd68dc278985fedb85946044bcb70dfa920
Pascal Vizeli <pvizeli@syshack.ch>
2020-04-25 11:17:03 +0200

    New approach for uboot and rpi 5.4 (#635)
    * New approach for uboot and rpi 5.4
    
    * fix dts
    
    * Update firmware to next

--- buildroot-external/board/raspberrypi/hassos-hook.sh
+++ buildroot-external/board/raspberrypi/hassos-hook.sh
@@ -7,6 +7,7 @@ function hassos_pre_image() {
     cp -t "${BOOT_DATA}" \
         "${BINARIES_DIR}/u-boot.bin" \
         "${BINARIES_DIR}/boot.scr"
+    cp "${BINARIES_DIR}"/*.dtb "${BOOT_DATA}/"
     cp -r "${BINARIES_DIR}/rpi-firmware/overlays" "${BOOT_DATA}/"
     cp "${BOARD_DIR}/../boot-env.txt" "${BOOT_DATA}/config.txt"
 
@@ -20,29 +21,6 @@ function hassos_pre_image() {
         "${BINARIES_DIR}/rpi-firmware/fixup.dat" \
         "${BINARIES_DIR}/rpi-firmware/start.elf" \
         "${BINARIES_DIR}/rpi-firmware/bootcode.bin"
-    fi
-
-    # DTS
-    if [[ "${BOARD_ID}" == "rpi" ]]; then
-        cp -t "${BOOT_DATA}" \
-        "${BINARIES_DIR}/rpi-firmware/bcm2708-rpi-b.dtb" \
-        "${BINARIES_DIR}/rpi-firmware/bcm2708-rpi-b-plus.dtb" \
-        "${BINARIES_DIR}/rpi-firmware/bcm2708-rpi-cm.dtb"
-    elif [[ "${BOARD_ID}" == "rpi0-w" ]]; then
-        cp -t "${BOOT_DATA}" \
-        "${BINARIES_DIR}/rpi-firmware/bcm2708-rpi-zero-w.dtb"
-    elif [[ "${BOARD_ID}" == "rpi2" ]]; then
-        cp -t "${BOOT_DATA}" \
-        "${BINARIES_DIR}/rpi-firmware/bcm2709-rpi-2-b.dtb" \
-        "${BINARIES_DIR}/rpi-firmware/bcm2710-rpi-2-b.dtb"
-    elif [[ "${BOARD_ID}" =~ "rpi3" ]]; then
-        cp -t "${BOOT_DATA}" \
-        "${BINARIES_DIR}/rpi-firmware/bcm2710-rpi-3-b.dtb" \
-        "${BINARIES_DIR}/rpi-firmware/bcm2710-rpi-3-b-plus.dtb" \
-        "${BINARIES_DIR}/rpi-firmware/bcm2710-rpi-cm3.dtb"
-    elif [[ "${BOARD_ID}" =~ "rpi4" ]]; then
-        cp -t "${BOOT_DATA}" \
-        "${BINARIES_DIR}/rpi-firmware/bcm2711-rpi-4-b.dtb"
     fi
 
     # Set cmd options
