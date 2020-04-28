f9515d654b3226b2d41dd633ae5f5fde02acc666
Pascal Vizeli <pvizeli@syshack.ch>
2020-04-28 08:28:31 +0000

    Fix commit message

--- scripts/update-kernel-rpi.sh
+++ scripts/update-kernel-rpi.sh
@@ -13,4 +13,4 @@ fi
 
 sed -i "s/BR2_LINUX_KERNEL_CUSTOM_TARBALL_LOCATION=\"https://github.com/raspberrypi/linux/.*\"/BR2_LINUX_KERNEL_CUSTOM_TARBALL_LOCATION=\"https://github.com/raspberrypi/linux/archive/$1.tar.gz\"/g" buildroot-external/configs/*
 sed -i "s/| Raspberry Pi\(.*\) | .* |/| Raspberry Pi\1 | $2 |/g" Documentation/kernel.md
-git commit -m "RaspberryPi: Update kernel $1" buildroot-external/configs/* Documentation/kernel.md
+git commit -m "RaspberryPi: Update kernel $2 - $1" buildroot-external/configs/* Documentation/kernel.md
