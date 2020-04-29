ddea6bd11795363dac260a8be333a6650d09ffeb
Pascal Vizeli <pvizeli@syshack.ch>
2020-04-29 08:42:04 +0000

    Fix script

--- scripts/update-kernel-odroid.sh
+++ scripts/update-kernel-odroid.sh
@@ -11,6 +11,6 @@ if [ -z "$2" ]; then
     exit 1
 fi
 
-sed -i "s/BR2_LINUX_KERNEL_CUSTOM_TARBALL_LOCATION=\"https://github.com/hardkernel/linux/.*\"/BR2_LINUX_KERNEL_CUSTOM_TARBALL_LOCATION=\"https://github.com/hardkernel/linux/archive/$1.tar.gz\"/g" buildroot-external/configs/*
+sed -i "s|BR2_LINUX_KERNEL_CUSTOM_TARBALL_LOCATION=\"https://github.com/hardkernel/linux/.*\"|BR2_LINUX_KERNEL_CUSTOM_TARBALL_LOCATION=\"https://github.com/hardkernel/linux/archive/$1.tar.gz\"|g" buildroot-external/configs/*
 sed -i "s/| Odroid\(.*\) | .* |/| Odroid\1 | $2 |/g" Documentation/kernel.md
 git commit -m "Odroid: Update kernel $2 - $1" buildroot-external/configs/* Documentation/kernel.md
