39e78a10143b7af9ed3560910d27235d1aaab9fd
Pascal Vizeli <pvizeli@syshack.ch>
2020-07-23 12:20:50 +0200

    fix lint

--- scripts/enter.sh
+++ scripts/enter.sh
@@ -8,6 +8,8 @@ COMMAND="${ARGS:-bash}"
 sudo mkdir -p "${CACHE_DIR}"
 sudo chown -R "${BUILDER_UID}:${BUILDER_GID}" "${CACHE_DIR}"
 sudo docker build -t hassos:local .
+
+# shellcheck disable=SC2086
 sudo docker run -it --rm --privileged \
   -v "$(pwd):/build" -v "${CACHE_DIR}:/cache" \
   -e BUILDER_UID="${BUILDER_UID}" -e BUILDER_GID="${BUILDER_GID}" \
