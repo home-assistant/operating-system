fa5d5625709db6a73498b3147e6698492583723c
Aman Karmani <aman@tmm1.net>
2020-10-26 18:05:30 -0700

    add rpi-eeprom

---
+++ buildroot-external/package/rpi-eeprom/rpi-eeprom.mk
@@ -0,0 +1,25 @@
+#############################################################
+#
+# rpi-eeprom
+#
+#############################################################
+RPI_EEPROM_VERSION = f24abcd305ab844dfba2ac92e52883056786ccab
+RPI_EEPROM_SITE = $(call github,raspberrypi,rpi-eeprom,$(RPI_EEPROM_VERSION))
+RPI_EEPROM_LICENSE = BSD-3-Clause
+RPI_EEPROM_LICENSE_FILES = LICENSE
+RPI_EEPROM_INSTALL_IMAGES = YES
+RPI_EEPROM_FIRMWARE_PATH = firmware/stable/pieeprom-2020-09-03.bin
+
+define RPI_EEPROM_BUILD_CMDS
+	$(@D)/rpi-eeprom-config $(@D)/$(RPI_EEPROM_FIRMWARE_PATH) --out $(@D)/default.conf
+	(cat $(@D)/default.conf | grep -v ^$$; echo USB_MSD_PWR_OFF_TIME=1500) > $(@D)/boot.conf
+	$(@D)/rpi-eeprom-config $(@D)/$(RPI_EEPROM_FIRMWARE_PATH) --config $(@D)/boot.conf --out $(@D)/pieeprom.upd
+	sha256sum $(@D)/pieeprom.upd | awk '{ print $$1 }' > $(@D)/pieeprom.sig
+endef
+
+define RPI_EEPROM_INSTALL_IMAGES_CMDS
+	$(INSTALL) -D -m 0644 $(@D)/pieeprom.sig $(BINARIES_DIR)/rpi-eeprom/pieeprom.sig
+	$(INSTALL) -D -m 0644 $(@D)/pieeprom.upd $(BINARIES_DIR)/rpi-eeprom/pieeprom.upd
+endef
+
+$(eval $(generic-package))
