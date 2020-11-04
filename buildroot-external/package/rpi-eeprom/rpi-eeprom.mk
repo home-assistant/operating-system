fd0bc3d4dc550b31099d8fa2a3cb166e7700bfff
Aman Karmani <aman@tmm1.net>
2020-11-03 16:59:49 -0800

    use 2020-10-28 beta eeprom
    this improves boot from USB and speeds up boot times.
    also includes sd card v1 boot reliability.
    see https://github.com/raspberrypi/rpi-eeprom/pull/246
    
    i removed the previous USB_MSD_PWR_OFF_TIME override since it is no longer required.
    added HDMI_DELAY=0 so hdmi display is always visible for debugging.

--- buildroot-external/package/rpi-eeprom/rpi-eeprom.mk
+++ buildroot-external/package/rpi-eeprom/rpi-eeprom.mk
@@ -3,18 +3,19 @@
 # rpi-eeprom
 #
 #############################################################
-RPI_EEPROM_VERSION = f24abcd305ab844dfba2ac92e52883056786ccab
+RPI_EEPROM_VERSION = 16bb29427f96dc8276a7102c0526154a1084bffd
 RPI_EEPROM_SITE = $(call github,raspberrypi,rpi-eeprom,$(RPI_EEPROM_VERSION))
 RPI_EEPROM_LICENSE = BSD-3-Clause
 RPI_EEPROM_LICENSE_FILES = LICENSE
 RPI_EEPROM_INSTALL_IMAGES = YES
-RPI_EEPROM_FIRMWARE_PATH = firmware/stable/pieeprom-2020-09-03.bin
+RPI_EEPROM_FIRMWARE_PATH = firmware/beta/pieeprom-2020-10-28.bin
 
 define RPI_EEPROM_BUILD_CMDS
 	$(@D)/rpi-eeprom-config $(@D)/$(RPI_EEPROM_FIRMWARE_PATH) --out $(@D)/default.conf
-	(cat $(@D)/default.conf | grep -v ^$$; echo USB_MSD_PWR_OFF_TIME=1500) > $(@D)/boot.conf
+	(cat $(@D)/default.conf | grep -v ^$$; echo HDMI_DELAY=0) > $(@D)/boot.conf
 	$(@D)/rpi-eeprom-config $(@D)/$(RPI_EEPROM_FIRMWARE_PATH) --config $(@D)/boot.conf --out $(@D)/pieeprom.upd
 	sha256sum $(@D)/pieeprom.upd | awk '{ print $$1 }' > $(@D)/pieeprom.sig
+	echo "ts: $$(date -u +%s)" >> $(@D)/pieeprom.sig
 endef
 
 define RPI_EEPROM_INSTALL_IMAGES_CMDS
