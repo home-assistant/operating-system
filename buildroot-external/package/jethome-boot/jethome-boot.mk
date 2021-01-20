9d1eaceb7ae797885c831fc007e64b3c09c95ac3
Vyacheslav Bocharov <devel@lexina.in>
2021-01-20 10:08:21 +0300

    Move uboot to jethome-boot package.

---
+++ buildroot-external/package/jethome-boot/jethome-boot.mk
@@ -0,0 +1,29 @@
+################################################################################
+#
+# jethome uboot loader
+#
+################################################################################
+
+JETHOME_BOOT_VERSION = HEAD
+JETHOME_BOOT_SITE = https://github.com/adeepn/jethub-prebuilt-u-boot.git
+JETHOME_BOOT_SITE_METHOD = git
+JETHOME_BOOT_INSTALL_IMAGES = YES
+JETHOME_BOOT_DEPENDENCIES = uboot
+JETHOME_BOOT_BINS = u-boot.bin u-boot.bin.usb.tpl
+
+
+ifeq ($(BR2_PACKAGE_JETHOME_BOOT_JETHUB_D1),y)
+JETHOME_BOOT_DEVICE = jethub-j100
+
+else ifeq ($(BR2_PACKAGE_JETHOME_BOOT_JETHUB_H1),y)
+JETHOME_BOOT_DEVICE = jethub-j80
+
+endif
+
+define JETHOME_BOOT_INSTALL_IMAGES_CMDS
+	$(foreach f,$(JETHOME_BOOT_BINS), \
+			cp -dpf $(@D)/hassos/$(JETHOME_BOOT_DEVICE)/$(f) $(BINARIES_DIR)/
+	)
+endef
+
+$(eval $(generic-package))
