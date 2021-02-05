7d2042a87b697caa89f2490b249efd615275e285
Stefan Agner <stefan@agner.ch>
2021-02-05 16:30:31 +0100

    RaspberryPi: Update firmware 0d458874a89921fbe460e422b239695e1e101e2b (#1211)
    * RaspberryPi: Update firmware 0d458874a89921fbe460e422b239695e1e101e2b
    
    * update-firmware-rpi.sh: Fix to work with new hash files

--- scripts/update-firmware-rpi.sh
+++ scripts/update-firmware-rpi.sh
@@ -21,7 +21,7 @@ rm -rf /tmp/rpi-firmware.tar.gz
 
 
 sed -i "s/+RPI_FIRMWARE_VERSION = [a-f0-9]*/+RPI_FIRMWARE_VERSION = $1/g" "$2"
-sed -i "s/+sha256 [a-f0-9]* rpi-firmware-[a-f0-9]*.tar.gz/+sha256 $checksum rpi-firmware-$1.tar.gz/g" "$2"
+sed -i "s/+sha256\s*[a-f0-9]*\s*rpi-firmware-[a-f0-9]*.tar.gz/+sha256  $checksum  rpi-firmware-$1.tar.gz/g" "$2"
 
 patch -d buildroot -p 1 < "$2"
 git commit -m "RaspberryPi: Update firmware $1" "$2" buildroot/package/rpi-firmware
