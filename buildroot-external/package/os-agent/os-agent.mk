392e56e84851d04d8af80f8a4dcc877f364c2b4e
Stefan Agner <stefan@agner.ch>
2021-03-12 17:42:32 +0100

    Do go vendoring as part of the buildroot build

--- buildroot-external/package/os-agent/os-agent.mk
+++ buildroot-external/package/os-agent/os-agent.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-OS_AGENT_VERSION = a8acb25adec4dc7d3a01c17c12bb88700ecb5ed1
-OS_AGENT_SITE = $(call github,agners,home-assistant-os-agent,$(OS_AGENT_VERSION))
+OS_AGENT_VERSION = 0cb548c6acfb7e0a9542e6c85ffbce626af04cce
+OS_AGENT_SITE = $(call github,home-assistant,os-agent,$(OS_AGENT_VERSION))
 OS_AGENT_LICENSE = Apache License 2.0
 OS_AGENT_LICENSE_FILES = LICENSE
 OS_AGENT_GOMOD = github.com/home-assistant/os-agent
@@ -16,5 +16,12 @@ define OS_AGENT_INSTALL_INIT_SYSTEMD
 	$(INSTALL) -D -m 0644 $(@D)/contrib/haos-agent.service \
 		$(TARGET_DIR)/usr/lib/systemd/system/haos-agent.service
 endef
+
+define OS_AGENT_GO_VENDORING
+	(cd $(@D); \
+		$(HOST_DIR)/bin/go mod vendor)
+endef
+
+OS_AGENT_POST_PATCH_HOOKS += OS_AGENT_GO_VENDORING
 
 $(eval $(golang-package))
