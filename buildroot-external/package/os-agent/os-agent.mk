ae0aeb84f513d877e402d38bd547555bd67f0b22
Stefan Agner <stefan@agner.ch>
2021-04-08 20:22:19 +0200

    Update to OS Agent 1.0.0 (#1317)
    * Update to OS Agent 1.0.0
    
    * Use new D-Bus path/interface/object in haos-wipe.service

--- buildroot-external/package/os-agent/os-agent.mk
+++ buildroot-external/package/os-agent/os-agent.mk
@@ -4,15 +4,16 @@
 #
 ################################################################################
 
-OS_AGENT_VERSION = 8ce70291d410b2d32bac5057e1ada70e8c49bf12
+OS_AGENT_VERSION = 1.0.0
 OS_AGENT_SITE = $(call github,home-assistant,os-agent,$(OS_AGENT_VERSION))
 OS_AGENT_LICENSE = Apache License 2.0
 OS_AGENT_LICENSE_FILES = LICENSE
 OS_AGENT_GOMOD = github.com/home-assistant/os-agent
+OS_AGENT_LDFLAGS = -X main.version=$(OS_AGENT_VERSION)
 
 define OS_AGENT_INSTALL_INIT_SYSTEMD
-	$(INSTALL) -D -m 0644 $(@D)/contrib/io.homeassistant.conf \
-		$(TARGET_DIR)/etc/dbus-1/system.d/io.homeassistant.conf
+	$(INSTALL) -D -m 0644 $(@D)/contrib/io.hass.conf \
+		$(TARGET_DIR)/etc/dbus-1/system.d/io.hass.conf
 	$(INSTALL) -D -m 0644 $(@D)/contrib/haos-agent.service \
 		$(TARGET_DIR)/usr/lib/systemd/system/haos-agent.service
 endef
