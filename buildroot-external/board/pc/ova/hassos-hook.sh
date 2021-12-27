24615739ea58234892b2fd6ff67a1edae6a09867
Stefan Agner <stefan@agner.ch>
2021-12-27 14:58:27 +0100

    Use LSI Logic SCSI controller (#1695)
    * Use LSI Logic SCSI controller in vmdk descriptor as well
    
    For some reason, the vmdk disk format's descriptor contains the
    controller type as well. By default, qemu-img sets it to "ide", which
    seems not optimal especially for VMware's ESXi. Set adapter type to
    commonly supported "lsilogic".
    
    * Move ova image generation to hdd-image.sh

--- buildroot-external/board/pc/ova/hassos-hook.sh
+++ buildroot-external/board/pc/ova/hassos-hook.sh
@@ -14,9 +14,7 @@ function hassos_pre_image() {
 
 
 function hassos_post_image() {
-    local HDD_IMG="$(hassos_image_name img)"
-    local HDD_OVA="$(hassos_image_name ova)"
-    local OVA_DATA="${BINARIES_DIR}/ova"
+    local hdd_img="$(hassos_image_name img)"
 
     # Virtual Disk images
     convert_disk_image_virtual
@@ -27,14 +25,8 @@ function hassos_post_image() {
     convert_disk_image_xz qcow2
 
     # OVA
-    mkdir -p "${OVA_DATA}"
-    rm -f "${HDD_OVA}"
-
-    cp -a "${BOARD_DIR}/home-assistant.ovf" "${OVA_DATA}/home-assistant.ovf"
-    qemu-img convert -O vmdk -o subformat=streamOptimized "${HDD_IMG}" "${OVA_DATA}/home-assistant.vmdk"
-    (cd "${OVA_DATA}" || exit 1; "${HOST_DIR}/bin/openssl" sha256 home-assistant.* >home-assistant.mf)
-    tar -C "${OVA_DATA}" --owner=root --group=root -cf "${HDD_OVA}" home-assistant.ovf home-assistant.vmdk home-assistant.mf
+    convert_disk_image_ova
 
     # Cleanup
-    rm -f "${HDD_IMG}"
+    rm -f "${hdd_img}"
 }
