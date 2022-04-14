aaf2bf83178f2d769e4a7ac943d1ea9af982e617
Stefan Agner <stefan@agner.ch>
2022-04-14 18:26:06 +0200

    Cleanup Buildroot package for Raspberry Pi Bluetooth support (#1846)
    * Replace bluetooth-bcm43xx with pi-bluetooth Buildroot package
    
    The new pi-bluetooth packages the scripts and systemd service from
    the Raspberry distribution package directly:
    https://github.com/RPi-Distro/pi-bluetooth
    
    * Update to latest pi-bluetooth service files
    
    * Update busybox configuration to 1.35.0
    
    The new/deleted configurations are generated automatically, no actual
    change in this patch.
    
    * Enable busybox xxd command
    
    The xxd tool is useful for conversion in scripts.
    
    * Prevent start erros on Compute Module 4 without WiFi/Bluetooth

---
+++ buildroot-external/package/pi-bluetooth/pi-bluetooth.mk
@@ -0,0 +1,25 @@
+################################################################################
+#
+# Pi-Bluetooth
+#
+################################################################################
+
+PI_BLUETOOTH_VERSION = 23af66cff597c80523bf9581d7f75d387227f183
+PI_BLUETOOTH_SITE = $(call github,RPi-Distro,pi-bluetooth,$(PI_BLUETOOTH_VERSION))
+PI_BLUETOOTH_LICENSE = BSD-3-Clause
+PI_BLUETOOTH_LICENSE_FILES = debian/copyright
+
+define PI_BLUETOOTH_INSTALL_TARGET_CMDS
+	$(INSTALL) -d $(TARGET_DIR)/etc/systemd/system/hassos-hardware.target.wants
+	$(INSTALL) -m 0644 $(BR2_EXTERNAL_HASSOS_PATH)/package/pi-bluetooth/hciuart.service $(TARGET_DIR)/usr/lib/systemd/system/
+	$(INSTALL) -m 0644 $(BR2_EXTERNAL_HASSOS_PATH)/package/pi-bluetooth/bthelper@.service $(TARGET_DIR)/usr/lib/systemd/system/
+
+	$(INSTALL) -d $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/usr/bin/btuart $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/usr/bin/bthelper $(TARGET_DIR)/usr/bin/
+
+	$(INSTALL) -d $(TARGET_DIR)/usr/lib/udev/rules.d
+	$(INSTALL) -m 0644 $(@D)/lib/udev/rules.d/90-pi-bluetooth.rules $(TARGET_DIR)/usr/lib/udev/rules.d/
+endef
+
+$(eval $(generic-package))
