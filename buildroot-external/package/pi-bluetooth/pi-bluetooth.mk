c83c4b58839ca2f898599a75f585cb53fac650d1
Stefan Agner <stefan@agner.ch>
2022-04-14 17:26:20 +0200

    Fix installation

--- buildroot-external/package/pi-bluetooth/pi-bluetooth.mk
+++ buildroot-external/package/pi-bluetooth/pi-bluetooth.mk
@@ -10,17 +10,17 @@ PI_BLUETOOTH_SITE = $(call github,RPi-Distro,pi-bluetooth,$(PI_BLUETOOTH_VERSION
 PI_BLUETOOTH_LICENSE = BSD-3-Clause
 PI_BLUETOOTH_LICENSE_FILES = debian/copyright
 
-define BLUETOOTH_BCM43XX_INSTALL_TARGET_CMDS
+define PI_BLUETOOTH_INSTALL_TARGET_CMDS
 	$(INSTALL) -d $(TARGET_DIR)/etc/systemd/system/hassos-hardware.target.wants
-	$(INSTALL) -m 0644 $(BR2_EXTERNAL_HASSOS_PATH)/hciuart.service $(TARGET_DIR)/usr/lib/systemd/system/
-	$(INSTALL) -m 0644 $(BR2_EXTERNAL_HASSOS_PATH)/bthelper@.service $(TARGET_DIR)/usr/lib/systemd/system/
+	$(INSTALL) -m 0644 $(BR2_EXTERNAL_HASSOS_PATH)/package/pi-bluetooth/hciuart.service $(TARGET_DIR)/usr/lib/systemd/system/
+	$(INSTALL) -m 0644 $(BR2_EXTERNAL_HASSOS_PATH)/package/pi-bluetooth/bthelper@.service $(TARGET_DIR)/usr/lib/systemd/system/
 
 	$(INSTALL) -d $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/usr/bin/btuart $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -m 0755 $(@D)/usr/bin/bthelper $(TARGET_DIR)/usr/bin/
 
 	$(INSTALL) -d $(TARGET_DIR)/usr/lib/udev/rules.d
-	$(INSTALL) -m 0644 $(@D)/usr/lib/udev/rules.d/90-pi-bluetooth.rules $(TARGET_DIR)/usr/lib/udev/rules.d/
+	$(INSTALL) -m 0644 $(@D)/lib/udev/rules.d/90-pi-bluetooth.rules $(TARGET_DIR)/usr/lib/udev/rules.d/
 endef
 
 $(eval $(generic-package))
