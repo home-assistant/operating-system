b614ceb2ba22c6fa112ba90b9ba3a166f67a77e5
Stefan Agner <stefan@agner.ch>
2022-09-07 19:21:30 +0200

    Bump Raspberry Pi kernel and firmware to 1.20220830 (#2117)
    * RaspberryPi: Update kernel 5.15.61 - 1.20220830
    
    * Add Yellow to the Raspberry Pi kernel update script
    
    * Bump Yellow to kernel 5.15.61 - 1.20220830
    
    Also drop the work around for the LED polarity as the new firmware
    has been fixed.
    
    * Explicitly select no kernel module compression
    
    Home Assistant OS uses a compressed rootfs already, no compression for
    kernel modules necessary.
    
    * Bump buildroot
    
    * buildroot d7e4c223e5...5468d36a26 (1):
      > package/rpi-firmware: bump version to 1.20220830

--- Documentation/kernel.md
+++ Documentation/kernel.md
@@ -6,12 +6,12 @@ Default Kernel tree: 5.15
 | Board | Version |
 |-------|---------|
 | Open Virtual Appliance | 5.15.65 |
-| Raspberry Pi | 5.15.32 |
-| Raspberry Pi 0-W | 5.15.32 |
-| Raspberry Pi 2 | 5.15.32 |
-| Raspberry Pi 3 | 5.15.32 |
-| Raspberry Pi 4 | 5.15.32 |
-| Home Assistant Yellow | 5.15.32 |
+| Raspberry Pi | 5.15.61 |
+| Raspberry Pi 0-W | 5.15.61 |
+| Raspberry Pi 2 | 5.15.61 |
+| Raspberry Pi 3 | 5.15.61 |
+| Raspberry Pi 4 | 5.15.61 |
+| Home Assistant Yellow | 5.15.61 |
 | Tinker Board | 5.15.65 |
 | Odroid-C2 | 5.15.65 |
 | Odroid-C4 | 5.15.65 |
--- scripts/update-kernel-rpi.sh
+++ scripts/update-kernel-rpi.sh
@@ -11,9 +11,9 @@ if [ -z "$2" ]; then
     exit 1
 fi
 
-defconfigs=(buildroot-external/configs/rpi*_defconfig)
+defconfigs=(buildroot-external/configs/{rpi*,yellow}_defconfig)
 sed -i "s|BR2_LINUX_KERNEL_CUSTOM_TARBALL_LOCATION=\"https://github.com/raspberrypi/linux/.*\"|BR2_LINUX_KERNEL_CUSTOM_TARBALL_LOCATION=\"https://github.com/raspberrypi/linux/archive/$1.tar.gz\"|g" "${defconfigs[@]}"
-sed -i "s/| Raspberry Pi\(.*\) | .* |/| Raspberry Pi\1 | $2 |/g" Documentation/kernel.md
+sed -i "s/| \(Raspberry Pi.*\|Home Assistant Yellow\) | .* |/| \1 | $2 |/g" Documentation/kernel.md
 git commit -m "RaspberryPi: Update kernel $2 - $1" "${defconfigs[@]}" Documentation/kernel.md
 
 ./scripts/check-kernel-patches.sh
