5891250bd13c0bdf1387f3c876de1c83f9d90a3d
Jens Maus <mail@jens-maus.de>
2023-11-21 09:52:03 +0100

    Bump rpi-rf-mod dt overlay to latest version (#2921)
    * Bump rpi-rf-mod dt overlay to latest version
    
    * updated to 1.13.1

--- buildroot-external/package/rpi-rf-mod/rpi-rf-mod.mk
+++ buildroot-external/package/rpi-rf-mod/rpi-rf-mod.mk
@@ -14,7 +14,7 @@
 #
 ################################################################################
 
-RPI_RF_MOD_VERSION = e7af99eeb4b8c480d36d5bc771484fd6d85188d6
+RPI_RF_MOD_VERSION = 0ee62f7a87d26e990fb0e03b00befb505a7e44fd
 RPI_RF_MOD_SITE = $(call github,jens-maus,RaspberryMatic,$(RPI_RF_MOD_VERSION))
 RPI_RF_MOD_LICENSE = Apache-2.0
 RPI_RF_MOD_LICENSE_FILES = LICENSE
