3260b0861c99c34e0ef204a3ccee1635405bb701
Stefan Agner <stefan@agner.ch>
2023-12-08 10:26:19 +0100

    Deploy kernel/device-tree into a RAUC slot specific directory
    This allows us to use the os_prefix feature to switch between slot A and
    B. Compared to the boot_partition option, this option allows to use a
    shared config.txt and cmdline.txt, which makes it more like how HAOS
    currently works on other Raspberry Pis.

--- buildroot-external/board/raspberrypi/rpi5-64/hassos-hook.sh
+++ buildroot-external/board/raspberrypi/rpi5-64/hassos-hook.sh
@@ -4,11 +4,12 @@
 function hassos_pre_image() {
     local BOOT_DATA="$(path_boot_dir)"
 
-    cp "${BINARIES_DIR}"/*.dtb "${BOOT_DATA}/"
-    cp "${BINARIES_DIR}"/Image "${BOOT_DATA}/kernel_2712.img"
+    mkdir -p "${BOOT_DATA}/slot-A/"
+    cp "${BINARIES_DIR}"/*.dtb "${BOOT_DATA}/slot-A/"
+    cp "${BINARIES_DIR}"/Image "${BOOT_DATA}/slot-A/kernel_2712.img"
+    cp "${BINARIES_DIR}"/*.dtbo "${BOOT_DATA}/slot-A/overlays/" 2>/dev/null || true
     cp "${BOARD_DIR}/config.txt" "${BOOT_DATA}/config.txt"
     cp "${BOARD_DIR}/cmdline.txt" "${BOOT_DATA}/cmdline.txt"
-    cp "${BINARIES_DIR}"/*.dtbo "${BOOT_DATA}/overlays/" 2>/dev/null || true
 }
 
 
