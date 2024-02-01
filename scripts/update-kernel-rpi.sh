b3e3d833a7cb568561fa4ddcc05c63cb41ee9fad
Jan Čermák <sairon@users.noreply.github.com>
2024-02-01 11:27:52 +0100

    Include links to kernel changelogs on kernel bump (#3133)
    Generate list of changelogs if we're bumping to the next patch release.
    If major or minor version is bumped, the commit message contains only
    the title, like before.
    
    Also fix English in the commit title of RPi bump.

--- scripts/update-kernel-rpi.sh
+++ scripts/update-kernel-rpi.sh
@@ -14,7 +14,7 @@ fi
 defconfigs=(buildroot-external/configs/{rpi*,yellow}_defconfig)
 sed -i "s|BR2_LINUX_KERNEL_CUSTOM_TARBALL_LOCATION=\"https://github.com/raspberrypi/linux/.*\"|BR2_LINUX_KERNEL_CUSTOM_TARBALL_LOCATION=\"https://github.com/raspberrypi/linux/archive/$1.tar.gz\"|g" "${defconfigs[@]}"
 sed -i "s/| \(Raspberry Pi.*\|Home Assistant Yellow\) | .* |/| \1 | $2 |/g" Documentation/kernel.md
-git commit -m "RaspberryPi: Update kernel $2 - $1" "${defconfigs[@]}" Documentation/kernel.md
+git commit -m "RaspberryPi: Update kernel to $2 - $1" "${defconfigs[@]}" Documentation/kernel.md
 
 ./scripts/check-kernel-patches.sh
 
