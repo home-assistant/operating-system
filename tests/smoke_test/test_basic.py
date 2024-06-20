6c7b6fdebe3fb5b5a7be21c13175e4851241473f
Jan Čermák <sairon@users.noreply.github.com>
2024-06-20 16:50:14 +0200

    Generate version information for RAUC when rauc.db is empty (#3436)
    RAUC currently doesn't know the version of the booted slot when booted for the
    first time or after wiping the data partition. As a result `ha os info` is
    missing this information too.
    
    As there's no built-in mechanism for generating these data by RAUC itself, add
    a oneshot service that checks if the boot slot information is contained in the
    rauc.db and if not, then generate it.
    
    RAUC seems to cope quite well even with bogus data contained in rauc.db but in
    any case, a test has been added to check that everything works as expected.

--- tests/smoke_test/test_basic.py
+++ tests/smoke_test/test_basic.py
@@ -36,11 +36,23 @@ def test_init(shell):
 
 
 @pytest.mark.dependency(depends=["test_init"])
-def test_rauc_status(shell):
-    output = shell.run_check("rauc status --output-format=shell")
+def test_rauc_status(shell, shell_json):
+    rauc_status = shell.run_check("rauc status --output-format=shell --detailed")
     # RAUC_BOOT_PRIMARY won't be set if correct grub env is missing
-    assert "RAUC_BOOT_PRIMARY='kernel.0'" in output
-    assert "rauc-WARNING" not in "\n".join(output)
+    assert "RAUC_BOOT_PRIMARY='kernel.0'" in rauc_status
+    assert "rauc-WARNING" not in "\n".join(rauc_status)
+
+    os_info = shell_json("ha os info --no-progress --raw-json")
+    expected_version = os_info.get("data", {}).get("version")
+    assert expected_version is not None and expected_version != ""
+
+    boot_slots = filter(lambda x: "RAUC_SYSTEM_SLOTS=" in x, rauc_status)
+    boot_slots = next(boot_slots, "").replace("RAUC_SYSTEM_SLOTS='", "").replace("'", "")
+    assert boot_slots != ""
+    booted_idx = boot_slots.split(" ").index("kernel.0")
+    assert booted_idx >= 0
+
+    assert f"RAUC_SLOT_STATUS_BUNDLE_VERSION_{booted_idx + 1}='{expected_version}'" in rauc_status
 
 
 @pytest.mark.dependency(depends=["test_init"])
