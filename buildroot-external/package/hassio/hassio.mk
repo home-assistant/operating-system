8cfa907c0cf9a366cc8373e8ee20781f0a71055b
Stefan Agner <stefan@agner.ch>
2024-11-18 14:14:30 +0100

    Fix channel in Supervisor updater.json config

--- buildroot-external/package/hassio/hassio.mk
+++ buildroot-external/package/hassio/hassio.mk
@@ -11,18 +11,18 @@ HASSIO_SITE = $(BR2_EXTERNAL_HASSOS_PATH)/package/hassio
 HASSIO_SITE_METHOD = local
 HASSIO_VERSION_URL = "https://version.home-assistant.io/"
 ifeq ($(BR2_PACKAGE_HASSIO_CHANNEL_STABLE),y)
-HASSIO_VERSION_FILE = "stable.json"
+HASSIO_VERSION_CHANNEL = "stable"
 else ifeq ($(BR2_PACKAGE_HASSIO_CHANNEL_BETA),y)
-HASSIO_VERSION_FILE = "beta.json"
+HASSIO_VERSION_CHANNEL = "beta"
 else ifeq ($(BR2_PACKAGE_HASSIO_CHANNEL_DEV),y)
-HASSIO_VERSION_FILE = "dev.json"
+HASSIO_VERSION_CHANNEL = "dev"
 endif
 
 HASSIO_CONTAINER_IMAGES_ARCH = supervisor dns audio cli multicast observer core
 
 define HASSIO_CONFIGURE_CMDS
 	# Deploy only landing page for "core" by setting version to "landingpage"
-	curl -s $(HASSIO_VERSION_URL)$(HASSIO_VERSION_FILE) | jq '.core = "landingpage"' > $(@D)/version.json
+	curl -s $(HASSIO_VERSION_URL)$(HASSIO_VERSION_CHANNEL)".json" | jq '.core = "landingpage"' > $(@D)/version.json
 endef
 
 define HASSIO_BUILD_CMDS
@@ -37,7 +37,7 @@ endef
 HASSIO_INSTALL_IMAGES = YES
 
 define HASSIO_INSTALL_IMAGES_CMDS
-	$(BR2_EXTERNAL_HASSOS_PATH)/package/hassio/create-data-partition.sh "$(@D)" "$(BINARIES_DIR)" "$(BR2_PACKAGE_HASSIO_CHANNEL)"
+	$(BR2_EXTERNAL_HASSOS_PATH)/package/hassio/create-data-partition.sh "$(@D)" "$(BINARIES_DIR)" "$(HASSIO_VERSION_CHANNEL)"
 endef
 
 $(eval $(generic-package))
