f169f78bb00a1cefeb00662b67d63c3ded06fb56
Stefan Agner <stefan@agner.ch>
2024-11-18 14:39:46 +0100

    Add Kconfig option for Supervisor channel (#3618)
    * Add Makefile variable for Supervisor channel
    
    Allow to set the release channel pre-installed Home Assistant components
    like Supervisor and add-on are fetched from. This channel is then also
    used at runtime.
    
    * Use choice instead of string variable
    
    * Fix channel in Supervisor updater.json config
    
    * Add newlines

--- buildroot-external/package/hassio/hassio.mk
+++ buildroot-external/package/hassio/hassio.mk
@@ -9,13 +9,20 @@ HASSIO_LICENSE = Apache License 2.0
 # HASSIO_LICENSE_FILES = $(BR2_EXTERNAL_HASSOS_PATH)/../LICENSE
 HASSIO_SITE = $(BR2_EXTERNAL_HASSOS_PATH)/package/hassio
 HASSIO_SITE_METHOD = local
-HASSIO_VERSION_URL = "https://version.home-assistant.io/stable.json"
+HASSIO_VERSION_URL = "https://version.home-assistant.io/"
+ifeq ($(BR2_PACKAGE_HASSIO_CHANNEL_STABLE),y)
+HASSIO_VERSION_CHANNEL = "stable"
+else ifeq ($(BR2_PACKAGE_HASSIO_CHANNEL_BETA),y)
+HASSIO_VERSION_CHANNEL = "beta"
+else ifeq ($(BR2_PACKAGE_HASSIO_CHANNEL_DEV),y)
+HASSIO_VERSION_CHANNEL = "dev"
+endif
 
 HASSIO_CONTAINER_IMAGES_ARCH = supervisor dns audio cli multicast observer core
 
 define HASSIO_CONFIGURE_CMDS
 	# Deploy only landing page for "core" by setting version to "landingpage"
-	curl -s $(HASSIO_VERSION_URL) | jq '.core = "landingpage"' > $(@D)/stable.json
+	curl -s $(HASSIO_VERSION_URL)$(HASSIO_VERSION_CHANNEL)".json" | jq '.core = "landingpage"' > $(@D)/version.json
 endef
 
 define HASSIO_BUILD_CMDS
@@ -23,14 +30,14 @@ define HASSIO_BUILD_CMDS
 	$(Q)mkdir -p $(HASSIO_DL_DIR)
 	$(foreach image,$(HASSIO_CONTAINER_IMAGES_ARCH),\
 		$(BR2_EXTERNAL_HASSOS_PATH)/package/hassio/fetch-container-image.sh \
-			$(BR2_PACKAGE_HASSIO_ARCH) $(BR2_PACKAGE_HASSIO_MACHINE) $(@D)/stable.json $(image) "$(HASSIO_DL_DIR)" "$(@D)/images"
+			$(BR2_PACKAGE_HASSIO_ARCH) $(BR2_PACKAGE_HASSIO_MACHINE) $(@D)/version.json $(image) "$(HASSIO_DL_DIR)" "$(@D)/images"
 	)
 endef
 
 HASSIO_INSTALL_IMAGES = YES
 
 define HASSIO_INSTALL_IMAGES_CMDS
-	$(BR2_EXTERNAL_HASSOS_PATH)/package/hassio/create-data-partition.sh "$(@D)" "$(BINARIES_DIR)"
+	$(BR2_EXTERNAL_HASSOS_PATH)/package/hassio/create-data-partition.sh "$(@D)" "$(BINARIES_DIR)" "$(HASSIO_VERSION_CHANNEL)"
 endef
 
 $(eval $(generic-package))
