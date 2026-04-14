dd49a44850f52211df71341315cdb80a1893cd33
Jens Maus <mail@jens-maus.de>
2026-04-14 10:46:04 +0200

    bump generic-raw-uart to 1.33 and add kernel 6.14+ patches.

--- buildroot-external/package/generic_raw_uart/generic_raw_uart.mk
+++ buildroot-external/package/generic_raw_uart/generic_raw_uart.mk
@@ -9,11 +9,11 @@
 # Uses parts of bcm2835_raw_uart.c
 # Copyright (c) 2015 eQ-3 Entwicklung GmbH
 # https://github.com/eq-3/occu/tree/master/KernelDrivers
-# https://github.com/jens-maus/RaspberryMatic/tree/master/buildroot-external/package/bcm2835_raw_uart
+# https://github.com/openccu/openccu/tree/master/buildroot-external/package/bcm2835_raw_uart
 #
 ################################################################################
 
-GENERIC_RAW_UART_VERSION = c377d91caeb24831fbd4e318b25807d5276ed4ad
+GENERIC_RAW_UART_VERSION = 803a94746f4528e276743502edcd45671b4980a9
 GENERIC_RAW_UART_SITE = $(call github,alexreinert,piVCCU,$(GENERIC_RAW_UART_VERSION))
 GENERIC_RAW_UART_LICENSE = GPL2
 GENERIC_RAW_UART_LICENSE_FILES = LICENSE
