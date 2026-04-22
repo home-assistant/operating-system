9b6b0149a9821ef7dfdd06740ace04905ad35cff
nyok92 <40342954+nyok92@users.noreply.github.com>
2026-04-22 10:53:30 +0200

    Update hassos-hook.sh

--- buildroot-external/board/radxa/rock5b/hassos-hook.sh
+++ buildroot-external/board/radxa/rock5b/hassos-hook.sh
@@ -3,29 +3,15 @@
 
 function hassos_pre_image() {
     local BOOT_DATA="$(path_boot_dir)"
-    local EFIPART_DATA="${BINARIES_DIR}/efi-part"
 
-    mkdir -p "${BOOT_DATA}/EFI/BOOT"
+    cp "${BINARIES_DIR}/boot.scr" "${BOOT_DATA}/boot.scr"
+    cp "${BINARIES_DIR}"/*.dtb "${BOOT_DATA}/"
 
-    cp "${BOARD_DIR}/grub.cfg" "${EFIPART_DATA}/EFI/BOOT/grub.cfg"
-    cp "${BOARD_DIR}/cmdline.txt" "${EFIPART_DATA}/cmdline.txt"
-    grub-editenv "${EFIPART_DATA}/EFI/BOOT/grubenv" create
-    grub-editenv "${EFIPART_DATA}/EFI/BOOT/grubenv" set ORDER="A B"
-    grub-editenv "${EFIPART_DATA}/EFI/BOOT/grubenv" set A_OK=1
-    grub-editenv "${EFIPART_DATA}/EFI/BOOT/grubenv" set A_TRY=0
-
-    cp -r "${EFIPART_DATA}/"* "${BOOT_DATA}/"
+    cp "${BOARD_DIR}/boot-env.txt" "${BOOT_DATA}/haos-config.txt"
+    cp "${BOARD_DIR}/cmdline.txt" "${BOOT_DATA}/cmdline.txt"
 }
 
 
 function hassos_post_image() {
-    convert_disk_image_virtual vmdk
-    convert_disk_image_virtual vdi
-    convert_disk_image_virtual qcow2
-
-    convert_disk_image_zip vmdk
-    convert_disk_image_zip vdi
-    convert_disk_image_xz qcow2
-
     convert_disk_image_xz
 }
