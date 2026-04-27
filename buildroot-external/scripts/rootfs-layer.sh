2b1a7f58887ba9a8b85ac8dc2fe43e36b660532d
nyok92 <40342954+nyok92@users.noreply.github.com>
2026-04-27 14:20:59 +0200

    Update rootfs-layer.sh

--- buildroot-external/scripts/rootfs-layer.sh
+++ buildroot-external/scripts/rootfs-layer.sh
@@ -23,7 +23,7 @@ function fix_rootfs() {
     rm -f "${TARGET_DIR}/usr/lib/systemd/system/sysinit.target.wants/systemd-update-done.service"
 
     # Fix: tempfs with /srv
-   # sed -i "/srv/d" "${TARGET_DIR}/usr/lib/tmpfiles.d/home.conf"
+    sed -i "/srv/d" "${TARGET_DIR}/usr/lib/tmpfiles.d/home.conf"
 
     # Fix: Could not generate persistent MAC address
     sed -i "s/MACAddressPolicy=persistent/MACAddressPolicy=none/g" "${TARGET_DIR}/usr/lib/systemd/network/99-default.link"
