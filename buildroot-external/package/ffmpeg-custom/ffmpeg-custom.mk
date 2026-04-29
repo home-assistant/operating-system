bc8993838acac1732764c5f7ab44f7159009fcca
nyok92 <40342954+nyok92@users.noreply.github.com>
2026-04-29 17:48:51 +0200

    Update ffmpeg-custom.mk

--- buildroot-external/package/ffmpeg-custom/ffmpeg-custom.mk
+++ buildroot-external/package/ffmpeg-custom/ffmpeg-custom.mk
@@ -11,15 +11,21 @@
 # https://code.ffmpeg.org/Kwiboo/FFmpeg.git -b v4l2-request-n8.0.1 ffmpeg \
 
 ifeq ($(BR2_PACKAGE_FFMPEG_CUSTOM),y)
-FFMPEG_VERSION= $(call qstrip,$(BR2_FFMPEG_CUSTOM_VERSION))
-FFMPEG_CUSTOM_TARBALL = $(call qstrip,$(BR2_FFMPEG_CUSTOM_TARBALL))
-FFMPEG_CUSTOM_CUSTOM_TARBALL_LOCATION = $(call qstrip,$(BR2_FFMPEG_CUSTOM_CUSTOM_TARBALL_LOCATION))
-https://code.ffmpeg.org/Kwiboo/FFmpeg/archive/v4l2request-v3.tar.gz
-FFMPEG_CUSTOM_REPO_URL = $(call qstrip,$(BR2_FFMPEG_CUSTOM_CUSTOM_REPO_URL))
+FFMPEG_SITE= $(call qstrip,$(BR2_PACKAGE_FFMPEG_CUSTOM_GIT_REPO))
+FFMPEG_VERSION= $(call qstrip,$(BR2_PACKAGE_FFMPEG_CUSTOM_GIT_REPO_VERSION))
 endif
 
-FFMPEG_VERSION= 3ac4e07593eeeeac8be43e3f6353161a8d1ce8be
-FFMPEG_SITE = $(call qstrip,$(BR2_FFMPEG_CUSTOM_CUSTOM_GIT))
 FFMPEG_SITE_METHOD = git
 FFMPEG_INSTALL_IMAGES = YES
 FFMPEG_INSTALL_TARGET = NO
+
+define HELLO_BUILD_CMDS
+    $(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D)
+endef
+
+define HELLO_INSTALL_TARGET_CMDS
+        $(INSTALL) -D -m 0755 $(@D)/hello $(TARGET_DIR)/usr/bin
+endef
+
+
+$(eval $(generic-package))
